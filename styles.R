2c86878101498199ff6254a395e46096c2961e6f
brianq-nisra <79097487+brianq-nisra@users.noreply.github.com>
2024-01-24 13:58:36 +0000

    Add files via upload

---
+++ styles.R
@@ -0,0 +1,9 @@
+style_page_title <- createStyle(fontSize = 14,
+                                textDecoration = "Bold")
+
+style_table_title <- createStyle(textDecoration = "Bold")
+
+style_table_header <- createStyle(textDecoration = "bold",
+                                  halign = "right")
+
+style_table_figures <- createStyle(numFmt = "#,##0")
